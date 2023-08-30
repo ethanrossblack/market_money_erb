8bcd8aa5d3f0914e1bed073b2ad8a22c56168cf3
Ethan Black <ethanrossblack@gmail.com>
2023-08-29 21:46:31 -0600

    feat: add vendors show route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :markets, only: [:index, :show] do
         get "/vendors", to: "market_vendors#index"
       end
+      resources :vendors, only: [:show]
     end
   end
 end
