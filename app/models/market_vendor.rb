765f6b3d605fbb7c18ce9daa101c8665c289cd26
Ethan Black <ethanrossblack@gmail.com>
2023-08-30 17:12:54 -0600

    add comments for later on the marketvendor model

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,18 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+
+  # validates_uniqueness_of: :market_id, scope: :vendor_id, message: "Message,,etc"
 end
+
+# begin
+  
+# rescue => exception
+#   controller``
+# end
+
+# rescue standardError
+
+
+
+#   valida
