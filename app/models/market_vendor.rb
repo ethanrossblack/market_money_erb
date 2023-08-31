ef8568ab5556d3133e35f3999621c498e62823f7
Ethan Black <ethanrossblack@gmail.com>
2023-08-31 16:15:49 -0600

    exceptions for market_vendor class

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -2,17 +2,6 @@ class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
 
-  # validates_uniqueness_of: :market_id, scope: :vendor_id, message: "Message,,etc"
+  validates :market_id, :vendor_id, presence: true
+  validates_uniqueness_of :market_id, scope: :vendor_id
 end
-
-# begin
-  
-# rescue => exception
-#   controller``
-# end
-
-# rescue standardError
-
-
-
-#   valida
