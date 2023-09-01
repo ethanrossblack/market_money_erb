d641f2751f9da0aa68d90ba8a238a01915eb1aed
Ethan Black <ethanrossblack@gmail.com>
2023-09-01 00:42:53 -0600

    feat: fix markets search results

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -10,11 +10,10 @@ class Api::V0::MarketsController < ApplicationController
   def search
     begin
       valid_params?(params)
-      search_results = Market.where("state LIKE ?", "%" + Market.sanitize_sql_like(params[:state]) + "%")
-      render json: MarketSerializer.new(search_results)
-      
-    rescue ActiveRecord::ActiveRecordError => exception
-      
+      results = search_results(params)
+      render json: MarketSerializer.new(results)
+    rescue ActiveRecord::StatementInvalid => e
+      render json: {"errors": [ {"detail": e.message}]}, status: 422
     end
   end
 
@@ -23,10 +22,50 @@ class Api::V0::MarketsController < ApplicationController
   def valid_params?(params)
     if params.has_key?(:state)
       true
-    elsif params.has_key?(:name) && !params.has_key(:state&&:city)
+    elsif params.has_key?(:name) && !params.has_key?(:state&&:city)
       true
     else
-      ActiveRecord::ActiveRecordError.new
+      raise ActiveRecord::StatementInvalid.new
     end
+  end
+
+  def search_params
+    require "pry"; binding.pry
+    params.permit(:name, :state, :city)
+  end
+
+  def search_results(params)
+    if params.has_key?(:state) && !params.has_key?(:name) && !params.has_key?(:city)
+      Market.where(
+        "lower(state) LIKE lower(:state)",
+        { state: params[:state] + "%" }
+      )
+    elsif params.has_key?(:state) && !params.has_key?(:name) && params.has_key?(:city)
+      pry
+      Market.where(
+        "lower(state) LIKE lower(:state) AND lower(city) LIKE lower(:city)",
+        { state: "#{params[:state]}%", city: "#{params[:city]}%" }
+      )
+    elsif params.has_key?(:state) && params.has_key?(:name) && !params.has_key?(:city)
+      require "pry"; binding.pry
+      Market.where(
+        "lower(state) LIKE lower(:state) AND lower(name) LIKE lower(:name)", 
+        { state: "#{params[:state]}%", name: params[:name] + "%" }
+      )
+    elsif !params.has_key?(:state) && params.has_key?(:name) && !params.has_key?(:city)
+      Market.where(
+        "lower(name) LIKE lower(:name)",
+        { name: params[:name] + "%" }
+      )
+    elsif params.has_key?(:state) && params.has_key?(:name) && params.has_key?(:city)
+      Market.where(
+        "lower(state) LIKE lower(:state) AND lower(name) LIKE lower(:name) AND lower(city) LIKE lower(:city)", 
+        { state: "#{params[:state]}%", name: "#{params[:name]}%", city: "#{params[:city]}%" }
+      )
+    end
+  end
+
+  def sanitize_term(term)
+    "%" + term + "%"
   end
 end
