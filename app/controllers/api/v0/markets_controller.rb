aaf06595725f8845dba0e1c34aeeb47058872866
Ethan Black <ethanrossblack@gmail.com>
2023-08-31 18:04:23 -0600

    feat: start of market search request

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -6,4 +6,27 @@ class Api::V0::MarketsController < ApplicationController
   def show
     render json: MarketSerializer.new(Market.find(params[:id]))
   end
+
+  def search
+    begin
+      valid_params?(params)
+      search_results = Market.where("state LIKE ?", "%" + Market.sanitize_sql_like(params[:state]) + "%")
+      render json: MarketSerializer.new(search_results)
+      
+    rescue ActiveRecord::ActiveRecordError => exception
+      
+    end
+  end
+
+  private
+
+  def valid_params?(params)
+    if params.has_key?(:state)
+      true
+    elsif params.has_key?(:name) && !params.has_key(:state&&:city)
+      true
+    else
+      ActiveRecord::ActiveRecordError.new
+    end
+  end
 end
